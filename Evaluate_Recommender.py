3872c2f7d51dbdb000d07438b2bb89b9eb40a2c0
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-10-23 13:08:52 +0200

    beautify code

--- Evaluate_Recommender.py
+++ Evaluate_Recommender.py
@@ -1,6 +1,3 @@
-# Implementation of a simple evaluation framework for recommender systems algorithms
-__author__ = 'mms'
-
 # Load required modules
 import csv
 import numpy as np
@@ -15,7 +12,6 @@ USERS_FILE = "./data/C1ku_idx_users.txt"            # user names for UAM
 
 NF = 10              # number of folds to perform in cross-validation TODO 10 folds
 
-
 # Function to read metadata (users or artists)
 def read_from_file(filename):
     data = []
@@ -28,12 +24,8 @@ def read_from_file(filename):
     f.close()
     return data
 
-
-# Function that implements a CF recommender. It takes as input the UAM, metadata (artists and users),
-# the index of the seed user (to make predictions for) and the indices of the seed user's training artists.
-# It returns a list of recommended artist indices
 def create_training_UAM(UAM, seed_uidx, seed_aidx_train):
-    # UAM               user-artist-matrix
+    # UAM               user-artist-matrix. Is changed by this function!
     # seed_uidx         user index of seed user
     # seed_aidx_train   indices in UAM of training artists for seed user
 
@@ -43,7 +35,6 @@ def create_training_UAM(UAM, seed_uidx, seed_aidx_train):
     # Remove information on test artists from seed's listening vector
     aidx_nz = np.nonzero(pc_vec)[0]                            # artists with non-zero listening events
     aidx_test = np.setdiff1d(aidx_nz, seed_aidx_train)         # compute set difference between all artist indices of user and train indices gives test artist indices
-#    print aidx_test
 
     # Set to 0 the listening events of seed user for testing (in UAM; pc_vec just points to UAM, is thus automatically updated)
     UAM[seed_uidx, aidx_test] = 0.0
@@ -66,10 +57,10 @@ def recommend_RB(artists_idx, no_items):
     return random_aidx
 
 
-# Function that implements a baseline recommender. It predicts the artists of a randomly chosen user 
+# Function that implements a baseline recommender. It uses the artists of a randomly chosen user as predictions.
 # It returns a list of recommended artist indices.
 def recommend_artists_from_random_user(u_aidx, all_other_users_idx, UAM, no_items):
-    # u_aidx               user artist indices
+    # u_aidx                 user artist indices
     # all_other_users_idx    indexes from where we choose a random user
     # no_items               no of items to predict
 
@@ -80,6 +71,7 @@ def recommend_artists_from_random_user(u_aidx, all_other_users_idx, UAM, no_item
 
     random_user_artists_idx = np.setdiff1d(random_user_artists_idx, u_aidx)
 
+    # Get randomly no_items number of artists
     random_user_artists_idx = random.sample(random_user_artists_idx, no_items)
 
     # Return list of recommended artist indices
@@ -99,11 +91,10 @@ if __name__ == '__main__':
     # Load UAM
     UAM = np.loadtxt(UAM_FILE, delimiter='\t', dtype=np.float32)
 
-    
-
     # For all users in our data (UAM)
     no_users = UAM.shape[0]
 
+    # Count number of users with spare data (users which have less than NF unique artists)
     no_sparse_users = 0
     for user in range(0, no_users):
         u_aidx = np.nonzero(UAM[user, :])[0]
--- Simple_Recommender_CF.py
+++ Simple_Recommender_CF.py
@@ -1,15 +1,11 @@
-# Implementation of a simple user-based CF recommender
-__author__ = 'mms'
-
 # Load required modules
 import csv
 import numpy as np
 
-
 # Parameters
-UAM_FILE = "./data/UAM_100.txt"                    # user-artist-matrix (UAM)
-ARTISTS_FILE = "./data/UAM_artists_100.txt"        # artist names for UAM
-USERS_FILE = "./data/UAM_users_100.txt"            # user names for UAM
+UAM_FILE = "./data/UAM.csv"                    # user-artist-matrix (UAM)
+ARTISTS_FILE = "./data/UAM_artists.csv"        # artist names for UAM
+USERS_FILE = "./data/UAM_users.csv"            # user names for UAM
 
 NEAREST_USERS = 3
 MAX_ITEMS_TO_PREDICT = 10
@@ -34,31 +30,32 @@ def simple_recommender_cf(user, UAM, max_items_to_predict, nearest_users_to_cons
 
     # Compute similarities as inner product between pc_vec of user and all users via UAM (assuming that UAM is already normalized)
     sim_users = np.inner(pc_vec, UAM)     # similarities between u and other users
-    # print sim_users
 
     # Sort similarities to all others
     sort_idx = np.argsort(sim_users)        # sort in ascending order
-#        print sort_idx
 
-    # Select the closest neighbor to seed user u (which is the last but one; last one is user u herself!)
-    #neighbor_idx = sort_idx[-2:-1][0] <- original TODO delete
+    # Select the closest neighbors to seed user u (which are the last but one; last one is user u herself!)
     neighbors_idx = sort_idx[-(nearest_users_to_consider+2):-2]
-    # for neighbor_idx in neighbors_idx:  # TODO beautify print output
+    # for neighbor_idx in neighbors_idx:
         # print "The closest user to user " + str(user) + " are " + str(neighbor_idx) + "."
         # print "The closest user to user " + users[u] + " is user " + users[neighbor_idx] + "."
 
-    # Get np.argsort(sim_users)l artist indices user u and her closest neighbor listened to, i.e., element with non-zero entries in UAM
+    # Get list of all neighbors' artist, except those artists from user u
     artist_idx_u = np.nonzero(UAM[user,:])                 # indices of artists user u listened to
-    user_artists_idx_n = { }
     artists = []
     for neighbor_idx in neighbors_idx:
         artist_idx_n = np.nonzero(UAM[neighbor_idx,:])[0].tolist()
-        user_artists_idx_n[neighbor_idx] = artist_idx_n
         artists += artist_idx_n
-    # artist_idx_n = np.nonzero(UAM[neighbor_idx,:])     # indices of artists user u's neighbor listened to TODO delete
+    
     artists = np.unique(artists)
     artists = np.setdiff1d(artists, artist_idx_u)
 
+    # Calculate artists' score
+    # calculated factors of the artists' scores:
+    #   user similarity
+    #   playcount of artist
+    #   number of users per artist
+    # could be further adjusted by adding additional weights
     artists_score = {}
     for artist in artists:
         user_artist_count = 0
@@ -74,38 +71,21 @@ def simple_recommender_cf(user, UAM, max_items_to_predict, nearest_users_to_cons
 
         artists_score[artist] *= float(user_artist_count) / len(neighbors_idx)
 
-    # calculated factors of the artists' scores:
-        # user similarity
-        # playcount of artist
-        # number of users per artist
-    # could be further adjusted by adding additional weights
-
+    # Sort the artists depending on their calculated scores
     sorted_recommended_artists = sorted(artists_score, key=artists_score.__getitem__, reverse=True)[:max_items_to_predict]
-
-
-    # for artist, score in artists_score.iteritems():
-        # print str(artist) + ": " + str(score)
     
     return sorted_recommended_artists
 
 # Main program
 if __name__ == '__main__':
-
-    # Initialize variables
-#    artists = []            # artists
-#    users = []              # users
-#    UAM = []                # user-artist-matrix
-
     # Load metadata from provided files into lists
     artists = read_from_file(ARTISTS_FILE)
     users = read_from_file(USERS_FILE)
-#    print users
-#    print artists
 
     # Load UAM
     UAM = np.loadtxt(UAM_FILE, delimiter='\t', dtype=np.float32)
 
-    # For all users
+    # For all users apply the simple recommender cf
     for user in range(0, UAM.shape[0]):
         # get playcount vector for current user u
         print "Next user recommendations: "
@@ -113,7 +93,7 @@ if __name__ == '__main__':
         for i in range(0, 10):
             print str(i+1) + ". rank: " + artists[user_most_listened_to_artists[i]]
         print "- - - - - - - "
-        recommended_artists_idx = simple_recommender_cv(user, UAM, MAX_ITEMS_TO_PREDICT, NEAREST_USERS)
+        recommended_artists_idx = simple_recommender_cf(user, UAM, MAX_ITEMS_TO_PREDICT, NEAREST_USERS)
         for i in range(0, len(recommended_artists_idx)):
             print str(i+1) + ". rank: " + artists[recommended_artists_idx[i]]
         print "\n"
